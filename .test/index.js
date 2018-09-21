b6641d6304209fd967780610e96a401ae3b9fba3
rektide de la faye <rektide@voodoowarez.com>
2018-09-21 12:27:16 -0400

    Add an incremental test for personal.

--- .test/index.js
+++ .test/index.js
@@ -1,4 +1,8 @@
+import PhasedMiddleware from ".."
 import Personal from "./personal/index.js"
+import { defaults as personalPipelines } from "./personal/pipeline.js"
+import Activity from "./personal/activity.js"
+import Meals from "./personal/meals.js"
 import Person from "./personal/person.js"
 import tape from "tape"
 
@@ -11,5 +15,21 @@ tape( "build and run a personal pipeline", function( t){
 	t.equal( person.energy, -4, "day tires a person out")
 	exec.night( person)
 	t.equal( person.energy, 10, "persons recover during the night")
+	t.end()
+})
+
+tape( "test a single pipeline of activity", function( t){
+	const
+	  phasedMiddleware= new PhasedMiddleware( personalPipelines),
+	  person= Person()
+
+	t.equal( person.energy, 10, "person starts with 10 nergy")
+	phasedMiddleware.exec.day( person)
+	t.equal( person.energy, 10, "nothing happens during the day with nothing installed")
+	phasedMiddleware.install(new Meals())
+	phasedMiddleware.exec.day( person)
+	t.equal( person.energy, 16, "person now eats meals during the day & gets energy")
+	phasedMiddleware.exec.night( person)
+	t.equal( person.energy, 20, "person now eats meals at night & gains energy")
 	t.end()
 })
--- .test/personal.js
+++
@@ -1,7 +0,0 @@
-const pipeline = new PhasedMiddleware({
-	pipelines: {
-		day: ["rise", "shine"],
-		night: ["retire", "rest"]
-	}
-})
-
