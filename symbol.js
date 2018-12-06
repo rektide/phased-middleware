85878fcb328c38f48fe779039f5c1ca2499cec79
rektide de la faye <rektide@voodoowarez.com>
2018-12-05 22:54:47 -0500

    Check in symbol.js file.

---
+++ symbol.js
@@ -0,0 +1,10 @@
+export const
+  middlewares= Symbol.for("phased-middleware:middlewares"),
+  Middlewares= middlewares,
+  $middlewares= middlewares,
+  name= Symbol.for("phased-middleware:name"),
+  Name= name,
+  $name= name,
+  pipelines= Symbol.for("phased-middleware:pipelines"),
+  Pipelines= pipelines,
+  $pipelines= pipelines
