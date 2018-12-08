5795fda58b9d1b65ba1015cb6fef17fb097c2f23
rektide de la faye <rektide@voodoowarez.com>
2018-12-08 14:40:29 -0500

    Attach phase-run pipelines directly object.
    Previously had 'pipeline' and 'exec' fields, but might as well
    expose the raw phased-run directly. Examples need updating.

--- phased-middleware.js
+++ phased-middleware.js
@@ -12,69 +12,15 @@ function _makePipeline( pipelineName){
 	return wrapper[ name]
 }
 
-// TODO: use field name in _phases when gathering
-function _phases( middleware){
-	const phases= {}
-	for( const desc of getAllDescriptors( middleware)){
-		const
-		  name= desc.name|| desc.symbol,
-		  member= middleware[ name]
-		let
-		  phase= member&& member.phase
-		if( !phase){
-			continue
-		}
-
-		// pluralize
-		if( !phase[Symbol.iterator]){
-			phase= [ phase]
-		}
-		for( let install of phase){
-			let
-			  pipeline= phases[ install.pipeline],
-			  destPhase
-
-			if( !pipeline){
-				pipeline= phases[ install.pipeline]= {[ install.phase]: [ member]}
-			}else if( destPhase= pipeline[ install.phase]){
-				 destPhase.push( member)
-			}else{
-				pipeline[ install.phase]= [ member]
-			}
-		}
-	}
-	return phases
-}
-
-function _prop(value, enumerable= false, refreshPipelines){
-	return {
-		get: function(){
-			return value
-		},
-		set: function( newValue){
-			const changed= value!= value
-			value= newValue
-			if( changed){
-				if( refreshPipelines){
-					this._pipeline= null
-					this._phaseNames= null
-				}
-				this.refresh()
-			}
-		},
-		enumerable
-	}
-}
-
 let nameSerial= 0
 function _middlewareName( middleware){
 	if( middleware.name){
-		return middleware.name
+		return `${middleware.name}-${nameSerial++}`
 	}
 	if( middleware.constructor){
 		return middleware.constructor.name
 	}
-	return `middleware-${nameSerial++}`
+	return `phased-middleware-${nameSerial++}`
 }
 
 function _defaultName(){
@@ -92,9 +38,7 @@ export class PhasedMiddleware{
 		this.exec= {}
 		this.pipeline= {}
 		for( let [ pipelineName, phases] of Object.entries( pipelines)){
-			const pipeline= new PhasedRun( phases)
-			this.pipeline=  pipeline
-			this.exec= _makeExec( pipeline, this)
+			this[ pipelineName]= new PhasedRun( phases)
 		}
 		if( middlewares){
 			this.install( ...middlewares)
@@ -102,6 +46,7 @@ export class PhasedMiddleware{
 	}
 	install( ...middlewares){
 		for( let middleware of middlewares){
+			// look for properties that have a `phase`
 			for( let descriptor of getAllDescriptors( middleware){
 				const handler= middleware[ descriptor.name]
 				let phases= handler.phase
@@ -112,9 +57,10 @@ export class PhasedMiddleware{
 					phases= [ phases]
 				}
 				for( let item of phases){
-					this.pipelines[ item.pipeline].install( item.phase, handler)
+					this[ item.pipeline].install( item.phase, handler)
 				}
 			}
+			// look at `phases` on the middleware
 			const middlewarePhases= middleware[ $phases]|| middleware.phases|| {}
 			for( let [ pipelineName, phases] in Object.entries( middlewarePhases)){
 				for( let [ phaseName, handlers] of Object.entries( phases)){
@@ -122,7 +68,7 @@ export class PhasedMiddleware{
 						handlers= [ handlers]
 					}
 					for( let handler of handlers){
-						this.pipelines[ pipelineName].install( phaseName, handler)
+						this[ pipelineName].install( phaseName, handler)
 					}
 				}
 			}
